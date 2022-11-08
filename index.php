bbea36017923254cc9a0eae79ad075d63f3af50e
guillaumeRequena <g.requena@laposte.net>
2022-11-08 20:31:35 +0100

    Creation of templates architecture

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <?php
 declare(strict_types=1);
 
+use App\Controller\RegisterController;
 use App\Core\Kernel;
 
 date_default_timezone_set('Europe/Paris');
 
 require_once 'vendor/autoload.php';
 
-$kernel = new Kernel();
-$kernel->process();
+$testTemplateController = new RegisterController();
+$testTemplateController->run();
+
+// $kernel = new Kernel();
+// $kernel->process();
